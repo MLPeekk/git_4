aa174b3bed00ae8c81d16a0ba1ba472f791cdd56
Aleksey Altukhov <avaltukhov7@mail.ru>
2024-02-24 11:07:17 +0300

    some change

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
         System.out.println(new Date());
     }
 }
